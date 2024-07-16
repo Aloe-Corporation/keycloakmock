f147c1a6340101685dd84b4399b5ab3a99fe411f
Florian Richard <florian.richard@aloe-corp.com>
2024-07-16 11:15:30 +0200

    Update error message

--- roles.go
+++ roles.go
@@ -2,7 +2,6 @@ package keycloakmock
 
 import (
 	"net/http"
-	"slices"
 
 	"github.com/gin-gonic/gin"
 )
@@ -39,15 +38,15 @@ func addRealmRoleToUser(conf Config) gin.HandlerFunc {
 			return
 		}
 
-		if !slices.ContainsFunc(conf.Roles, func(roleConf RolesConfig) bool {
-			if roleConf.ID.String() == *role.ID && roleConf.Name == *role.Name {
-				return true
-			}
-			return false
-		}) {
-			c.JSON(http.StatusBadRequest, "unknown role")
-			return
-		}
+		// if !slices.ContainsFunc(conf.Roles, func(roleConf RolesConfig) bool {
+		// 	if roleConf.ID.String() == *role.ID && roleConf.Name == *role.Name {
+		// 		return true
+		// 	}
+		// 	return false
+		// }) {
+		// 	c.JSON(http.StatusBadRequest, "unknown role")
+		// 	return
+		// }
 
 		c.JSON(http.StatusOK, "")
 	}
