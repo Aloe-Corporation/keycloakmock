7e1c9974a6be2bd066de6805ce405a0d7efc4470
Florian Richard <florian.richard@aloe-corp.com>
2024-07-18 15:54:38 +0200

    Fix flattenGroupConfig

--- groups.go
+++ groups.go
@@ -20,11 +20,13 @@ func getGroups(conf Config) gin.HandlerFunc {
 func flattenGroupConfig(groups []GroupConfig) []Group {
 	var result []Group
 	for _, group := range groups {
+		subGroups := flattenGroupConfig(group.SubGroup)
 		result = append(result, Group{
-			ID:   stringP(group.UUID.String()),
-			Name: stringP(group.Name),
+			ID:        stringP(group.UUID.String()),
+			Name:      stringP(group.Name),
+			SubGroups: &subGroups,
 		})
-		result = append(result, flattenGroupConfig(group.SubGroup)...)
+		// result = append(result, flattenGroupConfig(group.SubGroup)...)
 	}
 	return result
 }
