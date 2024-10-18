6cf7b46cb148bc5ee1d12ae2eb40447042691ffb
Florian Richard <florian.richard@aloe-corp.com>
2024-10-18 15:43:16 +0200

    Add error message on  getOrganizationsMembers

--- organizations.go
+++ organizations.go
@@ -9,13 +9,13 @@ import (
 func getOrganizationsMembers(conf Config) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if c.GetHeader("Authorization") == "" {
-			c.JSON(http.StatusUnauthorized, "")
+			c.JSON(http.StatusUnauthorized, "auth header is empty")
 			return
 		}
 
 		orgID := c.Param("org_id")
 		if orgID != conf.OrganizationId.String() {
-			c.JSON(http.StatusBadRequest, "")
+			c.JSON(http.StatusBadRequest, "invalid param org_id")
 			return
 		}
 
